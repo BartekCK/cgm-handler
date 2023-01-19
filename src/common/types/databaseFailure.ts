ad6658268f08cc43dca61f124469bdb84c8330fa
Bartek <>
2023-01-19 23:12:27 +0100

    feat: :sparkles: add cgm glucose table

--- src/common/types/databaseFailure.ts
+++ src/common/types/databaseFailure.ts
@@ -1,7 +1,7 @@
 import { FailureResult } from './result';
 
 export class DatabaseFailure extends FailureResult {
-    public constructor(errorMessage?: string, context?: unknown) {
-        super(errorMessage || '', 'DATABASE_FAILURE', 'INFRASTRUCTURE_ERROR', context);
+    public constructor(errorMessage: string, context?: unknown) {
+        super(errorMessage, 'DATABASE_FAILURE', 'INFRASTRUCTURE_ERROR', context);
     }
 }
