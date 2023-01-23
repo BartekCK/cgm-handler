15cf0e9139b3f3d1745800e812ea355ae9e3cffe
Bartek <>
2023-01-23 23:57:02 +0100

    chore: :guard: update test workflow

--- scripts/test_local_lambda.sh
+++ scripts/test_local_lambda.sh
@@ -1,9 +1,5 @@
 cd ..
-docker build --no-cache -t cgm-handler --platform linux/amd64 .
-docker run -d -p 9000:8080 --name cgm-handler-contaier cgm-handler:latest
+docker-compose up -d
+NODE_ENV=development npm run migrations:latest
 curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
-
-docker stop cgm-handler-contaier
-docker rm cgm-handler-contaier
-docker image rm cgm-handler
-
+docker-compose down
